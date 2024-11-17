6c64ff0bfe694c881b5e55a54fa7c2cfbaf3e649
Eselly Lopez Luna <eselly.lopez613@gmail.com>
2024-11-17 18:14:58 -0500

    added new function scheduleAfter

--- time.cpp
+++ time.cpp
@@ -4,10 +4,11 @@ email: eselly.lopezluna11@myhunter.cuny.edu
 course: CSCI 135
 date: Nov 2024
 instructor: Tong Yi
-assignment: Lab 10c
-program: This program adds new class type movie,
-enum type genre, and function printTimeSlot, 
-which prints the end time for a movie
+assignment: Lab 10D
+program: This program adds new function
+scheduleAfter, which produces and returns
+a new TimeSlot for the movie nextMovie,
+scheduled immediately after the time slot ts
 */
 
 #include <iostream>
@@ -20,41 +21,50 @@ public:
     int m;
 };
 
-enum Genre {ACTION, COMEDY, DRAMA, ROMANCE, THRILLER};
+enum Genre
+{
+    ACTION,
+    COMEDY,
+    DRAMA,
+    ROMANCE,
+    THRILLER
+};
 
-class Movie { 
-public: 
+class Movie
+{
+public:
     string title;
-    Genre genre;     
-    int duration;    
+    Genre genre;
+    int duration;
 };
 
-class Timeslot { 
-public: 
-    Movie movie;     
-    Time startTime;  
+class Timeslot
+{
+public:
+    Movie movie;
+    Time startTime;
 };
 
-int minutesSinceMidnight(Time time)
-{
-    int elapsed = 0;
-    elapsed += time.h * 60;
-    elapsed += time.m;
-    return elapsed;
-}
+// int minutesSinceMidnight(Time time)
+// {
+//     int elapsed = 0;
+//     elapsed += time.h * 60;
+//     elapsed += time.m;
+//     return elapsed;
+// }
 
-int minutesUntil(Time earlier, Time later)
-{
-    int dur_hr, dur_min = 0;
-    int total_dur;
-    dur_hr = later.h - earlier.h;
-    dur_min = later.m - earlier.m;
+// int minutesUntil(Time earlier, Time later)
+// {
+//     int dur_hr, dur_min = 0;
+//     int total_dur;
+//     dur_hr = later.h - earlier.h;
+//     dur_min = later.m - earlier.m;
 
-    total_dur = dur_hr * 60;
-    total_dur += dur_min;
+//     total_dur = dur_hr * 60;
+//     total_dur += dur_min;
 
-    return total_dur;
-}
+//     return total_dur;
+// }
 
 Time addMinutes(Time time0, int min)
 {
@@ -85,24 +95,42 @@ Time addMinutes(Time time0, int min)
     return time0;
 }
 
-void printMovie(Movie mv){
+void printMovie(Movie mv)
+{
     string g;
-    switch (mv.genre) {
-        case ACTION   : g = "ACTION"; break;
-        case COMEDY   : g = "COMEDY"; break;
-        case DRAMA    : g = "DRAMA";  break;
-        case ROMANCE  : g = "ROMANCE"; break;
-        case THRILLER : g = "THRILLER"; break;
+    switch (mv.genre)
+    {
+    case ACTION:
+        g = "ACTION";
+        break;
+    case COMEDY:
+        g = "COMEDY";
+        break;
+    case DRAMA:
+        g = "DRAMA";
+        break;
+    case ROMANCE:
+        g = "ROMANCE";
+        break;
+    case THRILLER:
+        g = "THRILLER";
+        break;
     }
     cout << mv.title << " " << g << " (" << mv.duration << " min)";
 }
 
-void printTimeSlot(Timeslot ts){
+void printTimeSlot(Timeslot ts)
+{
     Time end = addMinutes(ts.startTime, ts.movie.duration);
-    
-    cout << "[starts at " << ts.startTime.h <<":"<< ts.startTime.m
-     << ", ends by " << end.h << ":" << end.m << "]" <<endl;
-
+    printMovie(ts.movie);
+    cout << "[starts at " << ts.startTime.h << ":" << ts.startTime.m
+         << ", ends by " << end.h << ":" << end.m << "]" << endl;
+}
+Timeslot scheduleAfter(Timeslot ts, Movie nextMovie)
+{
+    Time end = addMinutes(ts.startTime, ts.movie.duration);
+    Timeslot next = {nextMovie, end};
+    return next;
 }
 
 int main()
@@ -110,17 +138,19 @@ int main()
     Movie movie1 = {"Back to the Future", COMEDY, 116};
     Movie movie2 = {"Black Panther", ACTION, 134};
     Movie movie3 = {"Hereditary", THRILLER, 127};
-    Movie movie4= {"Woman of the Hour", THRILLER, 95};
-    Timeslot morning = {movie1, {9, 15}};  
-    Timeslot late_morning = {movie4,{11,00}};
-    Timeslot daytime = {movie2, {12, 15}}; 
-    Timeslot evening = {movie2, {16, 45}}; 
-    Timeslot night = {movie3, {9,00}};
-    printTimeSlot(morning);
-    printTimeSlot(late_morning);
-    printTimeSlot(daytime);
-    printTimeSlot(evening);
-    printTimeSlot(night);
+    Movie movie4 = {"Woman of the Hour", THRILLER, 95};
+
+    Timeslot first = {movie1, {9, 15}};
+    Timeslot second = scheduleAfter(first, movie2);
+    Timeslot third = scheduleAfter(second, movie2);
+    Timeslot fourth = scheduleAfter(third, movie4);
+    Timeslot fifth = scheduleAfter(fourth, movie3);
+
+    printTimeSlot(first);
+    printTimeSlot(second);
+    printTimeSlot(third);
+    printTimeSlot(fourth);
+    printTimeSlot(fifth);
 
     return 0;
 }
